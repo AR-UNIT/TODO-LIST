014046fa0f315172b8cbb92a05ac32e4125e6818
AR-UNIT <baidyaarunit@gmail.com>
2024-12-23 14:30:09 -0500

    wrote clarifying comment on concurrent transaction execution guarantees against race conditions

--- DbQueryStrategies/DbQueryPostgresLockingStrategy.go
+++ DbQueryStrategies/DbQueryPostgresLockingStrategy.go
@@ -5,6 +5,21 @@ type PostgresRowLockingStrategy struct {
 	BasePostgresStrategy
 }
 
+// ONLY ROW LOCKING ON WRITES TO DB
+// THIS WILL NOT BLOCK ANY READS TO ROWS BEEN MODIFIED BY A CONCURRENT PROCESS USING A STANDARD SELECT
+// THIS ROW LOCKING STRATEGY IMPL IS ALSO USING STANDARD SELECT, ONLY ROW LOCKING FOR MODIFICATIONS
+// STALE READS COULD OCCUR if a row locked and modified by an update operation, is read using standard select
+// in Read Committed Isolation level in PostgresSql(default):
+/*
+ Transaction A locks and updates a row.
+ Transaction B reads the row while Transaction A still holds the lock but has not yet committed.
+ If Transaction A commits or rolls back, Transaction B may have seen stale or inconsistent data that
+	is no longer valid after the commit.
+
+	Problem with ReadCommitted isolation level is non-repeatable reads,
+		same row has different values when read at different points of time in the same transaction.
+*/
+
 func (prls *PostgresRowLockingStrategy) CompleteTask(id int) (int64, error) {
 	// Add row locking before completing the task
 	lockQuery := `SELECT id FROM TODO.tasks WHERE id = $1 FOR UPDATE`
